357ac7014412cb87af177a33969ba917a2102dd5
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-08 17:46:58 +0100

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let reverse = ''
 
-};
+    for(let i = str.length - 1; i >= 0; i--){
+        reverse += str[i];
+    }
+
+    return reverse;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
