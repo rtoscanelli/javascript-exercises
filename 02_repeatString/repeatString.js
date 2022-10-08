77a24141a49b870b79c408fc088e9640940fd12b
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-08 17:24:24 +0100

    Do exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    if (repeat < 0){
+        return 'ERROR'
+    }
+    
+    let res = '';
+    for (let i = 0; i < repeat; i++) {
+        res = res + str;
+    }
+    return res
 };
 
 // Do not edit below this line
