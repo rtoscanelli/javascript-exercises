a90daf4df199156c8b8d7b51f0c4d2ee88d5652a
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-25 19:42:23 +0100

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    
+    if (number < 0) { return 'OOPS'; }
+    
+    if (number == 0){
+        return 0;
+    }
+    if (number == 1) { 
+        return 1; 
+    }
+    else {
+        return fibonacci(number - 1) + fibonacci(number - 2); 
+    }
 };
 
 // Do not edit below this line
