ea3f4c6232c93cc9c418799ff9c23e3037b02df6
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-25 18:05:48 +0100

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+
+    if ((min < 0) || (max < 0)) { return 'ERROR'; }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) { return 'ERROR'}
+
+    let sum = 0;
+
+    if (min > max) {
+        const a = min;
+        min = max;
+        max = a;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
