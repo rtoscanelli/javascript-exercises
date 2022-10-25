5b9c4b08d29cf34625bbda85d5c3f23155a4e754
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-25 18:54:06 +0100

    Do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
+const add = function(n1, n2) {
+
+  return n1 + n2;
 	
 };
 
-const subtract = function() {
+const subtract = function(n1,n2) {
+
+  return n1 - n2;
 	
 };
 
-const sum = function() {
+const sum = function(numbers) {
+
+  let sum = 0;
+
+  for (const n in numbers) {
+    sum += numbers[n];
+  }
+
+return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
 
+  let mult = 1;
+
+  for (const n in numbers) {
+    mult *= numbers[n];
+  }
+
+  return mult;
 };
 
-const power = function() {
+const power = function(number, exp) {
+
+  return number ** exp;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+
+  let mult = 1;
+
+  for(let i = 1; i <= number; i++) {
+    mult *= i;
+  }
+
+  return mult;
 	
 };
 
