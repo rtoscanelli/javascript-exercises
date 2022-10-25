ab16557ade44656358c0976d7a9a54ef626134c7
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-25 19:29:45 +0100

    Do exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+const reverseString = function(str) {
+    
+    let reverse = ''
 
+    for(let i = str.length - 1; i >= 0; i--){
+        reverse += str[i];
+    }
+
+    return reverse;
+}
+
+const removeFromArray = function(array, ...args) {
+
+    return array.filter(x => !args.includes(x))
+};
+
+// Exercise:
+
+const palindromes = function (str) {
+
+    str = str.toLowerCase();
+    let str_list = str.split("");
+    const rev_str = reverseString(str);
+    let rev_str_list = rev_str.split("");
+    
+    str_list = removeFromArray(str_list, "!", ",", ".", "?", " ");
+    rev_str_list = removeFromArray(rev_str_list, "!", ",", ".", "?", " ");
+
+    for (const i in str_list) {
+        if (str_list[i] != rev_str_list[i]) { return false; }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
