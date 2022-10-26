5d6446058bc6c40f1a306380c6d86788c039880d
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-26 18:55:05 +0100

    Do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let maxAge = 0;
+    let oldestPerson = {};
+    let age = 0;
+
+    for (const i in people) {
+        if (!people[i].hasOwnProperty("yearOfDeath")) {
+            const date = new Date();
+            const currentYear = date.getFullYear();
+            age = currentYear - people[i]["yearOfBirth"];
+            if (age > maxAge) {
+                maxAge = age;
+                oldestPerson = people[i];
+            } 
+        }
+        else {
+            age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+            if (age > maxAge) {
+                maxAge = age;
+                oldestPerson = people[i];
+            }
+        } 
+    }
+
+    return oldestPerson;
 
 };
 
