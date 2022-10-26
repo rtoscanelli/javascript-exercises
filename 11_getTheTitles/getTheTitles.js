8cff21220e4e29ffffcb43203912c9e0fbd53921
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-26 18:38:49 +0100

    Do exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+    let titles = [];
+
+    for (const i in books) {
+        titles.push(books[i]["title"]);
+    }
+
+    return titles;
 
 };
 
