ec10634b23ac63cea619d93e975d345a80591c4d
rtoscanelli <ricardotoscanelli@gmail.com>
2022-10-28 12:32:50 +0100

    Do exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,30 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+
+    let codedStr = '';
+    let charUnicode = '';
+    let newChar = '';
+
+    for (const i in str) {
+        if ('a' <= str[i] && str[i] <= 'z') {
+            charUnicode = str.charCodeAt(i);
+            newChar = String.fromCharCode(((charUnicode - 96 + shift)%26 + 26)%26 + 96);
+            codedStr += newChar; 
+        }
+        else if ('A' <= str[i] && str[i] <= 'Z') {
+            charUnicode = str.charCodeAt(i);
+            newChar = String.fromCharCode(((charUnicode - 64 + shift)%26 + 26)%26 + 64);
+            codedStr += newChar;
+        }
+        else {
+            codedStr += str[i];
+        }
+    }
+
+    return codedStr;
 
 };
+
+console.log(caesar('Mjqqt, Btwqi!', -5));
 
 // Do not edit below this line
 module.exports = caesar;
